5b5ac0a38f419b1fd81a6f3dec04eee5bc88b992
Sharmillac <sharmillacordeiro9@hotmail.com>
2016-07-28 04:46:39 +0530

    Session 5

---
+++ circle-area.js
@@ -0,0 +1,6 @@
+  function calcCircleArea(radius)
+  {
+        
+        document.write("<P>The area of the circle of radius "+ radius +  " is " + (radius * radius * Math.PI) + "</p>");
+        
+  }
